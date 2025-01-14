b282ebc7dee21585f8b1ac0b133c1562c5ec0fd8
Jack Ho <grassgod@163.com>
2025-01-14 23:23:41 +0800

    上传模型文件

---
+++ app/run.py
@@ -0,0 +1,15 @@
+import os
+import sys
+from flask import Flask, request, jsonify
+
+# 创建flask应用
+app = Flask(__name__)
+
+
+@app.route('/process', methods=['GET'])
+def process():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    # 打开flask服务
+    app.run(host='0.0.0.0', port=5000, debug=True)
